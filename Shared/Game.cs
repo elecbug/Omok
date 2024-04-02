3a55bb5989f83ae994daf2a6d359f7139d3a9aa3
elecbug <deveb1479@gmail.com>
2024-04-02 21:59:32 +0900

    33 test

--- Shared/Game.cs
+++ Shared/Game.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Shared
 {
@@ -153,80 +152,76 @@ namespace Shared
                 return false;
             }
 
-            bool result = false;
+            bool result;
 
             Map[x, y] = color;
 
-            List<Point> case1 = new List<Point>();
-            List<Point> case2 = new List<Point>();
-            List<Point> case3 = new List<Point>();
-            List<Point> case4 = new List<Point>();
-
-            int point1 = Case1_33(x, y, case1);
-            int point2 = Case2_33(x, y, case2);
-            int point3 = Case3_33(x, y, case3);
-            int point4 = Case4_33(x, y, case4);
-
-            if (point1 + point2 + point3 + point4 >= 2)
-            {
-                result = true;
-            }
-            else if (point1 >= 1)
-            {
-                int sum = 0;
-
-                foreach (Point p in case1)
-                {
-                    sum += Case2_33(p.X, p.Y, new List<Point>());
-                    sum += Case3_33(p.X, p.Y, new List<Point>());
-                    sum += Case4_33(p.X, p.Y, new List<Point>());
-                }
-
-                result = sum >= 1;
-            }
-            else if (point2 >= 1)
-            {
-                int sum = 0;
-
-                foreach (Point p in case2)
-                {
-                    sum += Case1_33(p.X, p.Y, new List<Point>());
-                    sum += Case3_33(p.X, p.Y, new List<Point>());
-                    sum += Case4_33(p.X, p.Y, new List<Point>());
-                }
-
-                result = sum >= 1;
-            }
-            else if (point3 >= 1)
-            {
-                int sum = 0;
-
-                foreach (Point p in case3)
-                {
-                    sum += Case2_33(p.X, p.Y, new List<Point>());
-                    sum += Case1_33(p.X, p.Y, new List<Point>());
-                    sum += Case4_33(p.X, p.Y, new List<Point>());
-                }
-
-                result = sum >= 1;
-            }
-            else if (point4 >= 1)
-            {
-                int sum = 0;
-
-                foreach (Point p in case4)
-                {
-                    sum += Case2_33(p.X, p.Y, new List<Point>());
-                    sum += Case3_33(p.X, p.Y, new List<Point>());
-                    sum += Case1_33(p.X, p.Y, new List<Point>());
-                }
-
-                result = sum >= 1;
-            }
+            result = AgamottoEye3(color, x, y) >= 2;
 
             Map[x, y] = Color.Empty;
 
             return result;
+        }
+
+        private int AgamottoEye3(Color color, int xx, int yy)
+        {
+            int resultSum = 0;
+            List<Point> usedPoint = new List<Point>();
+            bool[] flag = { false, false, false, false };
+
+            for (int x = Math.Max(0, xx - 3); x < Math.Min(Map.GetLength(0), xx + 4); x++)
+            {
+                for (int y = Math.Max(0, yy - 3); y < Math.Min(Map.GetLength(1), yy + 4); y++)
+                {
+                    int result = 0;
+
+                    if (Map[x, y] != Color.Empty && usedPoint.FindIndex(p => p.X == x && p.Y == y) == -1)
+                    {
+                        continue;
+                    }
+
+                    Map[x, y] = color;
+
+                    List<Point> case1 = new List<Point>();
+                    List<Point> case2 = new List<Point>();
+                    List<Point> case3 = new List<Point>();
+                    List<Point> case4 = new List<Point>();
+
+                    int point1 = flag[0] ? 0 : Case1_33(x, y, case1);
+                    int point2 = flag[1] ? 0 : Case2_33(x, y, case2);
+                    int point3 = flag[2] ? 0 : Case3_33(x, y, case3);
+                    int point4 = flag[3] ? 0 : Case4_33(x, y, case4);
+
+                    result += point1 + point2 + point3 + point4;
+
+                    if (point1 == 1)
+                    {
+                        usedPoint.Concat(case1);
+                        flag[0] = true;
+                    }
+                    if (point2 == 1)
+                    {
+                        usedPoint.Concat(case2);
+                        flag[1] = true;
+                    }
+                    if (point3 == 1)
+                    {
+                        usedPoint.Concat(case3);
+                        flag[2] = true;
+                    }
+                    if (point4 == 1)
+                    {
+                        usedPoint.Concat(case4);
+                        flag[3] = true;
+                    }
+
+                    Map[x, y] = Color.Empty;
+
+                    resultSum += result;
+                }
+            }
+
+            return resultSum;
         }
 
         private int Case1_33(int x, int y, List<Point> points)
@@ -240,7 +235,7 @@ namespace Shared
 
             try
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     if (Map[x + i, y] == color)
                     {
@@ -255,7 +250,7 @@ namespace Shared
             catch { }
             try
             {
-                for (int i = -1; i > -3; i--)
+                for (int i = -1; i > -4; i--)
                 {
                     if (Map[x + i, y] == color)
                     {
@@ -269,7 +264,7 @@ namespace Shared
             }
             catch { }
 
-            if (sum >= 3)
+            if (sum >= 4)
             {
                 int point = 0;
 
@@ -298,7 +293,7 @@ namespace Shared
 
             try
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     if (Map[x, y + i] == color)
                     {
@@ -313,7 +308,7 @@ namespace Shared
             catch { }
             try
             {
-                for (int i = -1; i > -3; i--)
+                for (int i = -1; i > -4; i--)
                 {
                     if (Map[x, y + i] == color)
                     {
@@ -327,7 +322,7 @@ namespace Shared
             }
             catch { }
 
-            if (sum >= 3)
+            if (sum >= 4)
             {
                 int point = 0;
 
@@ -356,7 +351,7 @@ namespace Shared
 
             try
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     if (Map[x + i, y + i] == color)
                     {
@@ -371,7 +366,7 @@ namespace Shared
             catch { }
             try
             {
-                for (int i = -1; i > -3; i--)
+                for (int i = -1; i > -4; i--)
                 {
                     if (Map[x + i, y + i] == color)
                     {
@@ -385,7 +380,7 @@ namespace Shared
             }
             catch { }
 
-            if (sum >= 3)
+            if (sum >= 4)
             {
                 int point = 0;
 
@@ -414,7 +409,7 @@ namespace Shared
 
             try
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     if (Map[x + i, y - i] == color)
                     {
@@ -429,7 +424,7 @@ namespace Shared
             catch { }
             try
             {
-                for (int i = -1; i > -3; i--)
+                for (int i = -1; i > -4; i--)
                 {
                     if (Map[x + i, y - i] == color)
                     {
@@ -443,7 +438,7 @@ namespace Shared
             }
             catch { }
 
-            if (sum >= 3)
+            if (sum >= 4)
             {
                 int point = 0;
 
